9efc06670bf6a43f16412cde7c8ace241457e4d2
Andres Roque <andresconstaahn@gmail.com>
2019-12-10 22:10:47 -0400

    Proyecto Creado y funciones probadas

---
+++ FinalWEB2/FinalWEB2/Controllers/CONTACTOSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalWEB2.Models;
+
+namespace FinalWEB2.Controllers
+{
+    public class CONTACTOSController : Controller
+    {
+        private WEBFINAL2Entities db = new WEBFINAL2Entities();
+
+        // GET: CONTACTOS
+        public ActionResult Index()
+        {
+            return View(db.CONTACTOS.ToList());
+        }
+
+        // GET: CONTACTOS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACTOS cONTACTOS = db.CONTACTOS.Find(id);
+            if (cONTACTOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACTOS);
+        }
+
+        // GET: CONTACTOS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CONTACTOS/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CEDULA,NombreCompleto,FECHA")] CONTACTOS cONTACTOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CONTACTOS.Add(cONTACTOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cONTACTOS);
+        }
+
+        // GET: CONTACTOS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACTOS cONTACTOS = db.CONTACTOS.Find(id);
+            if (cONTACTOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACTOS);
+        }
+
+        // POST: CONTACTOS/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CEDULA,NombreCompleto,FECHA")] CONTACTOS cONTACTOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cONTACTOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cONTACTOS);
+        }
+
+        // GET: CONTACTOS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACTOS cONTACTOS = db.CONTACTOS.Find(id);
+            if (cONTACTOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACTOS);
+        }
+
+        // POST: CONTACTOS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CONTACTOS cONTACTOS = db.CONTACTOS.Find(id);
+            db.CONTACTOS.Remove(cONTACTOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FinalWEB2/FinalWEB2/Controllers/VISITASController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalWEB2.Models;
+
+namespace FinalWEB2.Controllers
+{
+    public class VISITASController : Controller
+    {
+        private WEBFINAL2Entities db = new WEBFINAL2Entities();
+
+        // GET: VISITAS
+        public ActionResult Index()
+        {
+            var vISITA = db.VISITA.Include(v => v.CONTACTOS);
+            return View(vISITA.ToList());
+        }
+
+        // GET: VISITAS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VISITA vISITA = db.VISITA.Find(id);
+            if (vISITA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vISITA);
+        }
+
+        // GET: VISITAS/Create
+        public ActionResult Create()
+        {
+            ViewBag.ContactoDeVisita = new SelectList(db.CONTACTOS, "ID", "CEDULA");
+            return View();
+        }
+
+        // POST: VISITAS/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ContactoDeVisita,NombreCompleto,Motivo,Fecha,HoraEntrada,HoraSalida")] VISITA vISITA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VISITA.Add(vISITA);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ContactoDeVisita = new SelectList(db.CONTACTOS, "ID", "CEDULA", vISITA.ContactoDeVisita);
+            return View(vISITA);
+        }
+
+        // GET: VISITAS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VISITA vISITA = db.VISITA.Find(id);
+            if (vISITA == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ContactoDeVisita = new SelectList(db.CONTACTOS, "ID", "CEDULA", vISITA.ContactoDeVisita);
+            return View(vISITA);
+        }
+
+        // POST: VISITAS/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ContactoDeVisita,NombreCompleto,Motivo,Fecha,HoraEntrada,HoraSalida")] VISITA vISITA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vISITA).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ContactoDeVisita = new SelectList(db.CONTACTOS, "ID", "CEDULA", vISITA.ContactoDeVisita);
+            return View(vISITA);
+        }
+
+        // GET: VISITAS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VISITA vISITA = db.VISITA.Find(id);
+            if (vISITA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vISITA);
+        }
+
+        // POST: VISITAS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VISITA vISITA = db.VISITA.Find(id);
+            db.VISITA.Remove(vISITA);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
